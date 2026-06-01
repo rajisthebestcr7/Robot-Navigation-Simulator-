42b6ebc89faa623047d70321806d104acc0a99b4
rajisthebestcr7 <rajisthebestcr7@gmail.com>
2026-06-01 15:18:35 -0500

    Add comments and code to hello_robot.py

--- hello_robot.py
+++ hello_robot.py
@@ -1,1 +1,11 @@
+# hello_robot.py
+# Author: Raj Modi
+# Date: June 2026
+# Description:
+# My first Python program for my robotics and automation learning journey.
+# This script displays a simple welcome message and marks the beginning
+# of my robotics engineering portfolio.
 
+print("Hello, Robotics World!")
+print("My name is Raj Modi.")
+print("I am learning Python for robotics and automation.")
